139ad16543fd0149ffd9c02d6a1cf647448ad1a0
gneris177 <gneris177@gmail.com>
2021-02-15 14:16:41 -0300

    Reformulando

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,20 @@
 const abrirMenu = document.querySelector('.icon-menu')
 const fecharMenu = document.querySelector('.icon-close')
-const menu = document.querySelector('.cabecalho-itens-mobile')
+const menu = document.querySelector('.menu-responsivo')
 
-const menuMobile = () => {
+const abrirMenuSeg = document.querySelector('.titulo-segmento')
+const menuSegmentos = document.querySelector('.menu-segmentos')
+
+const abrirSegResp = document.querySelector('.segmento-resp')
+const menuSegmentosResp = document.querySelector('.menu-segmentos-resp')
+
+function menuMobile() {
     menu.style.display = 'flex'
     abrirMenu.style.display = 'none'
     fecharMenu.style.display = 'block'
 }
 
-const fecha = () => {
+function fecha() {
     menu.style.display = 'none'
     abrirMenu.style.display = 'block'
     fecharMenu.style.display = 'none'
@@ -16,3 +22,29 @@ const fecha = () => {
 
 abrirMenu.onclick = () => menuMobile()
 fecharMenu.onclick = () => fecha()
+
+function abrirSegmentos() {
+    menuSegmentos.style.display = 'flex'
+    abrirMenuSeg.innerHTML = 'Segmentos🔻'
+}
+
+function fechaSegmentos() {
+    menuSegmentos.style.display = 'none'
+    abrirMenuSeg.innerHTML = 'Segmentos🔺'
+}
+
+abrirMenuSeg.onclick = () => abrirSegmentos()
+abrirMenuSeg.onmouseover = () => abrirSegmentos()
+abrirMenuSeg.onclick = () => fechaSegmentos()
+
+function abrirSegmentosResp() {
+    menuSegmentosResp.style.display = 'block'
+    abrirSegResp.innerHTML = 'Segmentos🔻'
+}
+
+function fecharSegmentosResp() {
+    menuSegmentosResp.style.display = 'none'
+    abrirSegResp.innerHTML = 'Segmentos🔺'
+}
+
+abrirSegResp.onclick = () => abrirSegmentosResp()
